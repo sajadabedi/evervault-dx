a1a3f5cfbaaebed192cc6b4d482b08e9b198dc26
Sajjad Abedi <sajadxabedi@gmail.com>
2024-11-21 11:07:02 +0200

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Evervault Log icon micro-interactions
+
+I've used `framer-motion` for the animations.
+
+It's included two animated icons for the Evervault Logs page.
+
+<img src="animate.gif" alt="Animated Icon" style="height: 50%; width: 50%; object-fit: contain;">
--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import { ReloadChartIcon } from './ReloadChartIcon'
 
 function App() {
   return (
-    <div className="h-screen w-screen justify-center items-center flex gap-4">
-      <ChartColumnIcon />
-      <ReloadChartIcon />
+    <div className="flex flex-col gap-4 h-screen w-screen justify-center items-center relative">
+      <div className="flex gap-4">
+        <ChartColumnIcon />
+        <ReloadChartIcon />
+      </div>
+      <p className="text-xs text-gray-500 absolute bottom-10">
+        Click on icons to see the animation
+      </p>
     </div>
   )
 }
